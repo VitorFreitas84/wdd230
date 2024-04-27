2fd170afaf5a3b1ec81ff02a8d2d377e36ddcefe
Vitor Hugo Santos de Freitas <vitor.hugo.freitas@hotmail.com>
2024-04-26 23:35:39 -0300

    main

--- week01/scripts/script.js
+++ week01/scripts/script.js
@@ -1,5 +1,5 @@
 // Example JavaScript (you can add more functionality as needed)
 document.addEventListener('DOMContentLoaded', function () {
     // Your JavaScript code here
-    console.log('Welcome to the Freitas Chamber of Commerce website!');
+    console.log('Welcome to the Providence Chamber of Commerce website!');
 });
