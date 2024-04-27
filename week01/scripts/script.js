30e01c87d080e69ffb4fab6ce510148fd53af6e7
Vitor Hugo Santos de Freitas <vitor.hugo.freitas@hotmail.com>
2024-04-26 23:27:58 -0300

    main

---
+++ week01/scripts/script.js
@@ -0,0 +1,5 @@
+// Example JavaScript (you can add more functionality as needed)
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code here
+    console.log('Welcome to the Freitas Chamber of Commerce website!');
+});
