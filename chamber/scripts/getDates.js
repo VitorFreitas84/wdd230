ba613c7953eabcd5df4a5f77f115e341905e7b53
Vitor Hugo Santos de Freitas <vitor.hugo.freitas@hotmail.com>
2024-05-04 22:31:27 -0300

    correct

--- chamber/scripts/getDates.js
+++
@@ -1,5 +0,0 @@
-//set copyright year
-document.getElementById("copyrightYear").textContent = new Date().getFullYear();
-
-//Fetch last modified date of the document
-document.getElementById("lastModified").textContent = document.lastModified;
