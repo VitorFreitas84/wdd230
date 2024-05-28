528248ecadcb321705a83118eff8e88aed574f79
Vitor Hugo Santos de Freitas <vitor.hugo.freitas@hotmail.com>
2024-05-28 18:48:59 -0300

    w06

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,20 +1,11 @@
-const modeButton = document.querySelector("#mode");
-const moon = document.querySelector("#darkBtn");
-const sun = document.querySelector("#lightBtn");
-const body = document.querySelector("body");
+// Last modified
+document.getElementById("lastModified").innerHTML = document.lastModified;
 
-modeButton.addEventListener("click", () => {
-    if (moon.style.display === "none") {
-        // If moon image is hidden, show moon and hide sun
-        moon.style.display = "inline";
-        sun.style.display = "none";
-        body.style.background = "#eee";
-        body.style.color = "#000";
-    } else {
-        // If sun image is hidden, show sun and hide moon
-        moon.style.display = "none";
-        sun.style.display = "inline";
-        body.style.background = "#000";
-        body.style.color = "#fff";
-    }
+// Hamburger Menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
 });
